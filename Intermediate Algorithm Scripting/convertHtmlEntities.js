da9bc844e4b5bc725411575d8ada0b992f5a2bab
Jonathan Puc <jonpucxyz@Jonathans-MacBook-Pro.local>
2017-10-26 13:26:42 +1100

    add solutions

---
+++ Intermediate Algorithm Scripting/convertHtmlEntities.js
@@ -0,0 +1,20 @@
+/* Task: Convert the characters &, <, >, " (double quote), and ' (apostrophe),
+in a string to their corresponding HTML entities. */
+
+function convertHTML(str) {
+  /* use replace method with regex and function that uses a
+     ternary operator and returns the according html entity*/
+  return str.replace(/[&<>"']/g, v =>
+  // chain ternary to until right match
+  v == "&"
+    ? "&amp;"
+    : v == "<"
+      ? "&lt;"
+      : v == ">"
+        ? "&gt;"
+        : v == '"'
+          ? "&quot;"
+          : "&apos;");
+}
+
+convertHTML("Dolce & Gabbana");
